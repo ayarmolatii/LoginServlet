64e9dd2866b4d343d0b2eb33be684cc0495fd167
Andrii Yarmolatii <ayarmolatii@gmail.com>
2024-10-27 23:31:29 +0200

    2nd

--- src/main/java/com/example/LoginServlet.java
+++ src/main/java/com/example/LoginServlet.java
@@ -1,1 +1,30 @@
-//
+package com.example;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    private static final String USERNAME = "admin";
+    private static final String PASSWORD = "password";
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        if (USERNAME.equals(username) && PASSWORD.equals(password)) {
+            out.println("<h2>Вхід успішний! Ласкаво просимо!</h2>");
+        } else {
+            out.println("<h2>Невірний логін або пароль.</h2>");
+        }
+    }
+}
